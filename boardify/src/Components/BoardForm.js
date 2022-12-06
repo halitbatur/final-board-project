8c268fe77db897dbcf22d5fd444195be29ddd119
Aland Abdullah <32212842+alandio@users.noreply.github.com>
2022-12-06 15:25:34 +0300

    Add eslint and prettier formatting

--- boardify/src/Components/BoardForm.js
+++ boardify/src/Components/BoardForm.js
@@ -1,15 +1,29 @@
 import React from "react";
 
 function BoardForm() {
-    return (
-        <div className="pt-20">
-          <form className="border-4 border-black mx-auto justify-center align-center max-w-sm md:max-w-md text-xl px-10 pb-10">
-          <h1 className="text-center py-5 font-bold">Board Creation</h1>
-            <input className="border-4 border-black placeholder:italic pl-2 placeholder:pl-1" type="text" name="Board-Title" placeholder="enter a board title"/> 
-          <input className="px-3 py-1 bg-black text-white hover:bg-red-800 hover:cursor-pointer hover:font-bold" type="submit" value="Submit" />
-          </form>
-        </div>
-    )
+  return (
+    <div className="pt-20">
+      <form
+        className="border-4 border-black mx-auto justify-center
+      align-center max-w-sm md:max-w-md text-xl px-10 pb-10"
+      >
+        <h1 className="text-center py-5 font-bold">Board Creation</h1>
+        <input
+          className="border-4 border-black placeholder:italic pl-2
+          placeholder:pl-1"
+          type="text"
+          name="Board-Title"
+          placeholder="enter a board title"
+        />
+        <input
+          className="px-3 py-1 bg-black text-white hover:bg-red-800
+          hover:cursor-pointer hover:font-bold"
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default BoardForm;
--- boardify/src/Components/ToggleButton.js
+++ boardify/src/Components/ToggleButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function ToggleButton() {
-    return (
-        <button className=" ml-32 mt-20 px-3 py-1 bg-black text-white hover:bg-red-800 hover:cursor-pointer hover:font-bold">Toggle</button>
-    )
-} 
+  return (
+    <button
+      className=" ml-32 mt-20 px-3 py-1 bg-black text-white
+    hover:bg-red-800 hover:cursor-pointer hover:font-bold"
+    >
+      Toggle
+    </button>
+  );
+}
 
 export default ToggleButton;
