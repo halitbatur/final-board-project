f9ad8c6b6899321c190a8a6724e5abdcfb657fcf
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-12-07 19:00:22 +0300

    Adding Tailwind css to the project file

--- final-board-project/src/App.js
+++ final-board-project/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 function App() {
   return (
