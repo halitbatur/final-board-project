6fdeaef963910a30a8b5dc66061c1d6dc9deb7cb
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-12-08 14:20:42 +0300

    Taskform has been created but need final functionality

--- final-board-project/src/Components/bodyComponents/Home.js
+++ final-board-project/src/Components/bodyComponents/Home.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import TaskForm from './TaskForm'
 
 export default function Home() {
   return (
+    <div>
     <div>Home</div>
+    {/* this board div is just a filler, later it will be filled */}
+    <div>board div</div>
+    <div>
+      <label>Add your task here</label>
+    <TaskForm/>
+    </div>
+    </div>
   )
 }
