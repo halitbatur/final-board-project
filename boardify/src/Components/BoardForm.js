6682203c23b2c70191a264438e38d44a3b40764c
Danah <osta.danah@gmail.com>
2022-12-08 02:26:56 +0200

    aded interactivity to Board Form + rendered Title

--- boardify/src/Components/BoardForm.js
+++ boardify/src/Components/BoardForm.js
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import BoardList from "./BoardList";
 
 function BoardForm() {
+  const [formInputs, setFormInputs] = useState({
+    title: "",
+  });
+
+  const handleChange = (e) => {
+    setFormInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const [boardInputs, setBoardInputs] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setBoardInputs((prev) => [...prev, formInputs]);
+    setFormInputs({ title: "" });
+    //e.target.reset()
+  };
+
+  //console.log(boardInputs);
+
   return (
+    <>
     <div className="pt-20">
       <form
+        onSubmit={handleSubmit}
         className="border-4 border-black mx-auto justify-center
       align-center max-w-sm md:max-w-md text-xl px-10 pb-10"
       >
         <h1 className="text-center py-5 font-bold">Board Creation</h1>
         <input
-          className="border-4 border-black placeholder:italic pl-2
-          placeholder:pl-1"
+          className="border-4 border-black placeholder:italic pl-2 placeholder:pl-1"
           type="text"
-          name="Board-Title"
+          name="title"
           placeholder="enter a board title"
+          value={formInputs.title}
+          onChange={handleChange}
         />
         <input
-          className="px-3 py-1 bg-black text-white hover:bg-red-800
-          hover:cursor-pointer hover:font-bold"
+          className="px-3 py-1 bg-black text-white hover:bg-red-800 hover:cursor-pointer hover:font-bold"
           type="submit"
           value="Submit"
         />
       </form>
     </div>
+    <div>
+      <BoardList board={boardInputs}  />
+    </div>
+    </>
   );
 }
 
