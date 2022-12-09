7202826e0382e934a54cd352d66255cd0c86034e
Hevar Tofiq <88142613+RageOfKurd@users.noreply.github.com>
2022-12-09 21:39:35 +0300

    delete board functionality been completed (#25)
    Co-authored-by: Aland Abdullah <alandio@users.noreply.github.com>

--- boardify/src/Components/BoardCard.js
+++ boardify/src/Components/BoardCard.js
@@ -1,19 +1,26 @@
 import React from "react";
 import TaskForm from "./TaskForm";
 
-function BoardCard({ board, onDelete ,boardId}) {
+function BoardCard({ board, onDelete}) {
 
+  
   return (
-    <div className="px-8 my-10 bg-red-800 md:mx-3 border-2 rounded-xl border-black mx-auto justify-center
-         align-center w-56 md:w-72">
+    <div className="px-8 my-10 bg-red-800 md:mx-3 border-2 rounded-xl
+      border-black mx-auto justify-center align-center w-56 md:w-72">
       <button
         className="border-2 border-black w-[50px] float-right bg-black text-white font-bold mt-4"
-        onClick={() => onDelete(boardId)}
+        onClick={() => onDelete(board.boardId)}
       >
         X
       </button>
       <h1 className="text-2xl font-bold text-white justify-center mx-auto py-3">{board.title}</h1>
       <TaskForm />
+      <div className="flex justify-between">
+
+        <h6>tasks:10</h6>
+        
+
+      </div>
     </div>
   );
 }
