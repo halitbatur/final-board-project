feed82e0429456f31429daab5c4cdef2ed6fc5ab
MaryyamSalah <maryyamslah20@gmail.com>
2022-12-10 13:25:48 +0300

    NavBar

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const About =()=>{
+
+    return (
+        <div>
+            <h3>this is about page</h3>
+        </div>
+    )
+}
---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home =()=>{
+
+    return (
+        <div>
+            <h3>this is Home Page</h3>
+        </div>
+    )
+}
