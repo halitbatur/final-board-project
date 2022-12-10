5247174e808704a2aa592cd000b0fdfe661752b3
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-12-10 10:11:02 +0300

    Completed about page

--- final-board-project/src/Components/bodyComponents/About.js
+++ final-board-project/src/Components/bodyComponents/About.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function About() {
-  return <div>
-    <h1>About</h1>
-    <p>This is a taking note website but in a creative way where all notes are saved on your acccount with a lot more features</p>
-    <div className="flex flex-row">
+  return <div className="flex flex-col text-center">
+    <h1 className="text-lg font-bold m-5">About</h1>
+    <p className="m-5 text-2xl">This is a taking note website but in a creative way where all notes are saved on your acccount with a lot more features</p>
+    <div className="flex flex-row justify-around m-5">
     <div><a href="https://github.com/BaraaNazar" target="_blank">Baraa Nazar</a></div>
     <div><a href="https://github.com/3lawi964" target="_blank">Ali Majid</a></div>
     <div><a href="https://github.com/noor-ridha" target="_blank">Noor Rida</a></div>
