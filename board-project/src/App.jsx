e0c5a07869c04f0663127c3f427738af9c19ceea
Sakar Masoud <masoudsakar@gmail.com>
2022-12-10 17:03:52 +0300

    Sakar

--- board-project/src/App.jsx
+++ board-project/src/App.jsx
@@ -1,7 +1,9 @@
+import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Header from './components/header/Header';
 import Kanban from "./pages/kanban/Kanban";
 import MainTable from "./pages/mainTable/MainTable";
+import BoardCard from './components/BoardCard';
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
         <Route path="/" element={ <MainTable/> } />
         <Route path="/kanban" element={ <Kanban/> } />
       </Routes>
+      <BoardCard/>
     </div>
   );
 }
