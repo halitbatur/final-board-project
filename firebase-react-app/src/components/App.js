38592377da635101615e212ed17177bef770a04f
Nina Hawari <nina.hawari@gmail.com>
2022-12-10 21:44:32 +0200

    list item

--- firebase-react-app/src/components/App.js
+++ firebase-react-app/src/components/App.js
@@ -1,23 +1,16 @@
 import React from "react";
 import {Routes,Route,Link} from "react-router-dom";
 import Home from "./Home"
-import InProject from "./InProject"
+import List from "./List"
 import NavBar from "./NavBar"
 import { collection, addDoc, onSnapShot } from "firebase/firestore";
 import db from "../firebase";
 import "../styles/App.css";
 
 function App(){
-useEffect(()=>{
-    const snapshot = onSnapShot(collection(db, "users"))
-    console.log(snapshot);
-})
 return ( <div>
-<Routes>
-<Route path="/NavBar" element={<NavBar />}/>
-<Route path="/home" element={<Home />}/>
-<Route path="/Inproject" element={<InProject />}/>
-</Routes>
+    <h1>hi</h1>
+    <List />
 </div>
 )}
 
---
+++ firebase-react-app/src/components/List.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Paper, Typography } from "@material-ui/core";
+
+function List () {
+    return (
+        <div>
+            <Paper>
+                <Typography>
+                    Create Project
+                </Typography>
+            </Paper>
+        </div>
+    )
+}
+
+export default List;
