d33ca6531a8afe87beee62670f5d9252123d1965
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-12-12 09:24:19 +0300

    adding a new board

--- final-board-project/src/Components/forms/BoardForm.js
+++ final-board-project/src/Components/forms/BoardForm.js
@@ -1,45 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 // import {firebase} from "../../utils/firebase"
-import { doc, setDoc } from "firebase/firestore";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 // import { async } from "@firebase/util";
 import db from "../../utils/firebase";
 
 export default function BoardForm(props) {
-  async function addingNotes() {
-    await setDoc(doc(db, "fianl-board-project", "bbbbb"), {
-      name: "Los Angeles",
+  async function addingNotes(title) {
+    await addDoc(collection(db, "boards"), {
+      name: title,
       state: "CA",
       country: "USA",
     });
   }
-  useEffect(() => {
-    addingNotes();
-  }, []);
 
-  // const [boards, setBoards] = useState([]);
-  // const [loading, setLoading] = useState(false);
+  const [title, setTitle] = useState('')
 
-  // const ref = firebase.firestore().collection("fianl-board-project")
 
-  // useEffect(()=>{
+  const saveDoc = (e) => {
+    e.preventDefault()
 
-  // },[])
-  // if(loading){
-  //   return <h1>Loading...</h1>
-  // }
-
+    addingNotes(title)
+  }
+  
   return props.trigger ? (
-    <div className="w-full max-w-xs absolute top-1/2 translate-y-1/2 z-10 backdrop-blur-3xl">
-      <form className="bg-white shadow-md rounded px-8 pt-20 pb-8 mb-4 relative">
+    <div className="absolute z-10 w-full max-w-xs translate-y-1/2 top-1/2 backdrop-blur-3xl">
+      <form className="relative px-8 pt-20 pb-8 mb-4 bg-white rounded shadow-md" onSubmit={saveDoc}>
         <button
           onClick={() => props.setTrigger(false)}
           type="button"
-          className="bg-white absolute top-0 right-0 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          className="absolute top-0 right-0 inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
         >
           <span className="sr-only">Close menu</span>
           <svg
-            className="h-6 w-6"
+            className="w-6 h-6"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -56,16 +50,16 @@ export default function BoardForm(props) {
         </button>
         <div className="mb-4">
           <input
+            onChange={(e) => setTitle(e.target.value)}
             type="text"
             placeholder="title"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
-          "
+            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline "
             id="username"
           />
         </div>
         <button
           type="submit"
-          className="px-4 py-2 bg-indigo-50 outline-none border border-indigo-100 rounded text-indigo-500 font-medium active:scale-95 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed transition-colors duration-200"
+          className="px-4 py-2 font-medium text-indigo-500 transition-colors duration-200 border border-indigo-100 rounded outline-none bg-indigo-50 active:scale-95 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed"
         >
           Add
         </button>
