e5eb602369dddf975ee609f95cb79e888f2be1ce
heyam <heyam976@gmail.com>
2022-12-13 17:13:01 +0200

    firebase

--- notegame/src/index.js
+++ notegame/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {collection, addDoc,onSnapshot} from "firebase/firestore";
+import db from "./firebase";
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,6 +19,46 @@ root.render(
  </React.StrictMode>
 
 );
+
+function firebaseC(){
+   const [newNoteInput,setNewNoteInput]=useState({});
+   const[Note,setNote]=useState({});
+
+ useEffect(()=>{
+    onSnapshot(collection(db,"board"),(snapshot)=>{
+       snapshot.docChanges().forEach((docChange)=>{
+          if(docChanges.type==="added"){
+             setNote((prevNote)=>[...prevNote,docChange.doc.data()]);
+          }else if(docChange.type==="removed"){
+            setNote(
+               Note.filter((board)=>board.id!==docChange.doc.id))
+          }
+       })
+   });
+},[]);
+ const handleSubmit =async(event)=>{
+    event.preventDefault();
+    setNote((prev)=>{
+       return [newNote,...prev];
+    });
+   const docRef = await addDoc(collection(db, "board"), {
+      ...newNote,
+  
+   });
+   setNewNoteInput({
+      Active:"",
+      Date:"",
+      Done:"",
+      DueDate:"",
+      Text:"",
+      Title:"",
+      To:""
+
+   });
+}
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
