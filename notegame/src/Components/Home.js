7e4aac9247d0025e7cd6e53e69f23d274a2a88f3
iamyahia <yahiabaiz@gmail.com>
2022-12-13 20:25:39 +0000

    add the new layout button configuration home design'

--- notegame/src/Components/Home.js
+++ notegame/src/Components/Home.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import Card from "./Card";
+import Form from "./Form";
 function Home() {
-return (
-    <div>
-        <p>
-            Home Page 
-        </p>
-    </div>
-)
+  return (
+    <div className="container my-12 mx-auto px-4 md:px-12">
+      <h1 className="text-3xl font-bold underline">Home page</h1>
+      <br />
+      <br />
+      <br />
 
+      <Form />
+      {/* Cards */}
+      <Card />
+    </div>
+  );
 }
-export default  Home;
+export default Home;
--- notegame/src/Components/NavBar.js
+++ notegame/src/Components/NavBar.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 import "./NavBar.css";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-
-export default  function NavBar() {
-    return (
-       <>
-       <nav className="navBarContainer">
+export default function NavBar() {
+  return (
+    <>
+      {/* <nav className="navBarContainer">
         <h1>Recipes</h1>
             <ul>
                  <li>
@@ -20,8 +19,81 @@ export default  function NavBar() {
                 </li>
               
             </ul>
-       </nav>
-       </>
-    )
-}
+       </nav> */}
 
+      <nav class="navBarContainer shadow-xl p-3 border-gray-200 rounded bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+        <div class="container flex flex-wrap items-center justify-between mx-auto">
+          <a href="#" class="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              class="h-6 mr-3 sm:h-10"
+              alt="Flowbite Logo"
+            />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              kabanan board
+            </span>
+          </a>
+          <button
+            data-collapse-toggle="navbar-solid-bg"
+            type="button"
+            class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-solid-bg"
+            aria-expanded="false"
+          >
+            <span class="sr-only">Open main menu</span>
+            <svg
+              class="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+            <ul class="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
