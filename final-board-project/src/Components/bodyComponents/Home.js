e0923ae36594c096e1b31da3e00a6bd7f6c5c7c4
alimajid <alidzk98@gmail.com>
2022-12-13 14:25:33 +0300

    onsnapshot bug solved ✔

--- final-board-project/src/Components/bodyComponents/Home.js
+++ final-board-project/src/Components/bodyComponents/Home.js
@@ -1,56 +1,21 @@
 import React from "react";
 import BoardForm from "../forms/BoardForm";
-import { useState } from "react";
-// import { doc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
 import db from "../../utils/firebase";
-import { useEffect } from "react";
 import Board from "./Board";
 import { Link } from "react-router-dom";
 import Spinner from "./Spinner";
-import {
-  collection,
-  getDocs,
-  queryEqual,
-  QuerySnapshot,
-} from "firebase/firestore";
-import { query, where, onSnapshot, doc } from "firebase/firestore";
-import { async } from "@firebase/util";
-
-// import { async } from "@firebase/util";
+import { collection, onSnapshot } from "firebase/firestore";
 
 export default function Home() {
   const [boards, setBoards] = useState(null);
-
-  async function getData() {
-    const boardsCollection = collection(db, "boards");
-    getDocs(boardsCollection).then((res) => {
-      const result = res.docs.map((doc) => ({
-        data: doc.data(),
-        id: doc.id,
-      }));
-      setBoards(result);
-    });
-    // const q = query(collection(db, "boards"));
-    // const unsubscribe = onSnapshot(collection(db, "boards"), async (doc) => {
-    //   // console.log(
-    //   //   doc._snapshot.docChanges[0].doc.data.value.mapValue.fields.name
-    //   //     .stringValue
-    //   // );
-    //   const result = doc._snapshot.docChanges.map((ele) => {
-    //     return ele.doc.data.value.mapValue.fields.name.stringValue;
-    //   });
-    //   // console.log(result);
-    //   return result;
-    // });
-    // setBoards(unsubscribe);
-  }
-  useEffect(() => {
-    getData();
-  }, []);
-
-  useEffect(() => {
-    console.log(boards);
-  }, [boards]);
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "boards"), (snapshot) =>
+        setBoards(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+      ),
+    []
+  );
 
   const [boardClick, setBoardClick] = useState(false);
   return (
@@ -68,7 +33,7 @@ export default function Home() {
           boards.map((ele) => {
             return (
               <Link to={`/tasks/${ele.id}`} className="h-48 w-48" key={ele.id}>
-                <Board props={ele.data} />
+                <Board props={ele} />
               </Link>
             );
           })
