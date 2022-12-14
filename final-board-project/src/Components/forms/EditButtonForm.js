e6dd9453d10c99be52cbb180c5f3676d642b82dc
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-12-14 21:27:27 +0300

    Add to Done text

--- final-board-project/src/Components/forms/EditButtonForm.js
+++ final-board-project/src/Components/forms/EditButtonForm.js
@@ -60,7 +60,7 @@ function EditButtonForm({ taskId, onClose }){
             type="submit"
             className="px-4 py-2 font-medium text-indigo-500 transition-colors duration-200 border border-indigo-100 rounded outline-none bg-indigo-50 active:scale-95 hover:bg-indigo-400 hover:text-white focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed"
             >
-            Add
+            Done
             </button>
         </form>
         </div>
