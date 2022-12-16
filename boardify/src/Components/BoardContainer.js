80bb064c20bb23b60d69c1a133de6561726d6ffb
banelmikhael <banel.mikhael.bm@gmail.com>
2022-12-16 14:04:11 +0300

    finished completed tasks functionality- no design

--- boardify/src/Components/BoardContainer.js
+++ boardify/src/Components/BoardContainer.js
@@ -4,7 +4,7 @@ import BoardCard from "./BoardCard";
 function BoardContainer({ boards, onDelete }) {
     
   return (
-    <div className="sm:border-0 md:border-2 md:border-black mb-10 md:px-10 mx-10 rounded-[1%] min-h-[550px] grid md:gap-x-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="sm:border-0 md:border-2 md:border-black mb-10 md:px-10 mx-10 rounded-[1%] min-h-[550px] grid md:gap-x-3 md:grid-cols-2 lg:grid-cols-3 ">
       {boards.map((board) => {
         return (
             <BoardCard  key={board.boardId} board={board} onDelete={onDelete} />
--- boardify/src/Components/FinishedTasks.js
+++ boardify/src/Components/FinishedTasks.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import TaskCard from "./TaskCard";
+import FinishedTaskCard from "./FinishedTasksCard";
 import db from "../backend/firebase-config";
 import { collection, onSnapshot } from "firebase/firestore";
 
 const FinishedTasks = () => {
   const [boards, setBoards] = useState([]);
-  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
     onSnapshot(collection(db, "boards"), (snapshot) => {
@@ -19,36 +18,29 @@ const FinishedTasks = () => {
     });
   }, []);
 
-  useEffect(() => {
-    boards.map((board) => {
-      onSnapshot(
-        collection(db, "boards", board.boardId, "tasks"),
-        (snapshot) => {
-          snapshot.docChanges().forEach((docChange) => {
-            if (docChange.type === "added") {
-              const taskId = docChange.doc.id;
-              const taskObj = { ...docChange.doc.data(), taskId };
-              setTasks((prevTasksList) => [...prevTasksList, taskObj]);
-            }
-          });
-        }
-      );
-    });
-  }, [boards]);
-
-  console.log(boards);
-  console.log(boards.tasks);
-
   return (
     <div
       className="sm:border-0 md:border-2 mt-8 md:border-black mb-10 md:px-10
       mx-10 rounded-[1%] min-h-[550px] grid md:gap-x-3 md:grid-cols-2
       lg:grid-cols-3 xl:grid-cols-4"
     >
-      {tasks.map((task) => {
+      {boards.map((board) => {
         return (
-          <div>
-            <h1>hello mr</h1>
+          <div> 
+            <h1 className=" text-2xl text-center"> {board.title} </h1>
+            {board.tasks.map((task) => { 
+              // console.log(task.isCompleted)
+              return(
+              task.isCompleted ?
+              
+              <FinishedTaskCard
+               task={task}
+              /> 
+              :  
+              null
+         
+              )
+            })}
           </div>
         );
       })}
