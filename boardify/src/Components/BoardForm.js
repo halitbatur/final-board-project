80cb494fa218bdd69bd8ac291f24eb1135e4a0c7
Hevar Tofiq Hama <rageofkurd98@gmail.com>
2022-12-16 22:42:49 +0300

    done

--- boardify/src/Components/BoardForm.js
+++ boardify/src/Components/BoardForm.js
@@ -3,13 +3,13 @@ import React from "react";
 function BoardForm({onSubmitt,onChange,newBoard}) {
  
   return (
-    <div className="mt-6 mx-80 card rounded-xl bg-[#2c2c2c] shadow-xl">
+    <div className="mt-6 mx-80 card max-w-xl  rounded-xl bg-[#2c2c2c] shadow-xl">
       <form
-        className="  mx-auto justify-center
+        className="   mx-auto justify-center
       align-center max-w-sm md:max-w-md text-xl px-10 pb-10 mt-5"
       onSubmit={onSubmitt}
       >
-        <h1 className="text-center py-5 font-bold text-[#b5b5b5]">Board Creation</h1>
+        <h1 className="text-center  py-5 font-bold text-[#b5b5b5]">Board Creation</h1>
         <input
           className="w-52 md:w-64 border-2 rounded-md bg-[#EFEFEF] border-[#343434] placeholder:font-extralight text-[#616161] text-base p-2 pl-2
           placeholder:pl-1"
