1daa86fed68fc24b2f3387259225b73bc367d232
NanorGosdanian <nanorgosdanian@gmail.com>
2022-12-16 21:49:34 +0200

    added main page

--- board-project/src/Components/Sidebar.js
+++ board-project/src/Components/Sidebar.js
@@ -112,7 +112,7 @@ function Sidebar() {
                   d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"
                 ></path>
               </svg>
-              <span className="ml-2" >Products</span>
+              <span className="ml-2" >Main Page</span>
             </Link>
             <a
               href=""
@@ -131,6 +131,7 @@ function Sidebar() {
                   // clip-rule="evenodd"
                 ></path>
               </svg>
+              
               <span className="ml-2">Reports</span>
             </a>
             <a
--- board-project/src/pages/About.js
+++ board-project/src/pages/About.js
@@ -1,12 +1,31 @@
 import React from "react";
+import logo from './logo.png'
+import image1 from './image1.jpeg'
 
 function About() {
   return (
-    <div className="pt-20">
-      <h1 className="text-center py-5 font-bold text-2xl">
-        This is the About page
-      </h1>
+    <>
+    <div className="logo">
+      <img src={logo} alt="Logo" />
+      <button className="rounded shadow-lg my-4 w-full bg-gray-900 text-white font-bold p-2 hover:bg-gray-300
+          hover:cursor-pointer hover:text-white">Login</button>
+          <button className="rounded shadow-lg my-4 w-full bg-lime-500 text-white font-bold p-2 hover:bg-lime-300
+          hover:cursor-pointer hover:text-white" >Sign Up</button>
     </div>
+    <div className="paragraph">
+      <p><b>TaskGuru helps teams move work forward.</b></p>
+      <span>Collaborate, manage projetcs, and reach new productivity peaks. From high rises to the home office, the way your team works in unique-accomplish it all with TaskGuru </span>
+      <div>
+      <input type="email" id="email" name="email"></input>
+      <button className="rounded shadow-lg my-4 w-full bg-lime-500 text-white font-bold p-2 hover:bg-lime-300
+          hover:cursor-pointer hover:text-white" >Sign up-it's free!</button></div>
+          <img src={image1} alt="Image" />
+    </div>
+    <div className="footer paragrpagh">
+      <p><b>It's more than work, it's a way of working together.</b></p>
+      <span>Start with a TaskGuru board, lists, and cards. Customize and expand with more features as your teamwork grows. Manage projects, organize tasks, and build team spirit- all in one place.</span>
+    </div>
+    </>
   );
 }
 
