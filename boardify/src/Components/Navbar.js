a5ec7b0eff52ca31f9d01728fdd3eb8b9b2243a9
Hevar Tofiq Hama <rageofkurd98@gmail.com>
2022-12-16 16:04:04 +0300

    navbar is isolated into a seperate component

--- boardify/src/Components/Navbar.js
+++ boardify/src/Components/Navbar.js
@@ -3,30 +3,29 @@ import logo from '../bored-logo.png'
 
 function Navbar() {
 
-    return( <header
-        className="flex justify-between px-10 items-center text-xs md:text-base
+  return (
+    <header
+      className="flex justify-between px-10 items-center text-xs md:text-base
       space-x-10 md:space-x-36 bg-[#262626] text-[#ADADAD] h-16 transition"
-      >
-        <div className="logo container w-28 pt-2">
-          <img src={logo} alt="" className="">
-
-          </img>
-        </div>
-<div className="links flex gap-8  font-bold pr-10">
+    >
+      <div className="logo container w-28 pt-2">
+        <img src={logo} alt="" className="">
+        </img>
+      </div>
+      <div className="links flex gap-8  font-bold pr-10">
         <Link className="hover:text-[#E6E6E6]  transition" to="/">
           Home
         </Link>
-        <Link className="hover:text-[#E6E6E6] transition" to="/">
+        <Link className="hover:text-[#E6E6E6] transition" to="/finished">
           Finished Tasks
         </Link>
         <Link className="hover:text-[#E6E6E6]  transition" to="/about">
           About
         </Link>
-</div>
+      </div>
+    </header>
+  );
 
-       
-      </header>);
-    
 }
 
-export default Navbar ; 
+export default Navbar;
