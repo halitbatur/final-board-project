0b5b78971476599279914c6b1aba5a195a973d34
Aland Abdullah <32212842+alandio@users.noreply.github.com>
2022-12-17 10:03:23 +0000

    Make about page take more than 90 of screen height

--- boardify/src/Components/About.js
+++ boardify/src/Components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
 	return (
-		<section className="bg-[#EFEFEF]">
+		<section className="bg-[#EFEFEF] min-h-[90vh]">
 			<div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
 				<div className="mx-auto mb-8 max-w-screen-sm lg:mb-16">
 					<h2 className="mb-4 text-4xl tracking-tight font-extrabold text-[#2f2f2f] ">Our team ⚡</h2>
